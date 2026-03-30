b6d14bfc24edb42a34f65d84a5d8ee6858b12af5
github user name <sensha5172@gmail.com>
2026-03-31 07:09:00 +0900

    update

---
+++ footer.php
@@ -0,0 +1,14 @@
+    <?php echo basename(__FILE__); ?>
+    <!-- ===============================================
+       フッター
+       =============================================== -->
+    <footer class="footer_area">
+        <div class="footer_wrapper">
+            <img class="footer_logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="ロゴ" />
+            <div class="footer_text">Web Entertainment Design Inc.<br />West Building 3F<br />9-99 Sakuragaokacho Shibuya-ku<br />Tokyo, Japan 150-0031<br />T/99-9999-9999</div>
+        </div>
+        <p class="copyright">© 2024 株式会社階段式. All rights reserved.</p>
+    </footer>
+    </body>
+
+    </html>
---
+++ header.php
@@ -0,0 +1,35 @@
+<?php echo basename(__FILE__); ?>
+
+<!doctype html>
+
+<!-- 変更 -->
+<html <?php language_attributes(); ?>>
+
+<head>
+    <!-- 変更 -->
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- 変更 -->
+    <title><?php wp_title('|', true, 'right');
+            bloginfo('name'); ?>
+    </title>
+    <!-- 変更 -->
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <header class="header_area">
+        <h1 class="header_logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="ロゴ" />
+        </h1>
+        <nav class="header_nav">
+            <ul class="header_list">
+                <li class="header_item"><a href="#" class="header_link">NEWS</a></li>
+                <li class="header_item"><a href="#" class="header_link">ABOUT</a></li>
+                <li class="header_item"><a href="#" class="header_link">BUSINESS</a></li>
+                <li class="header_item"><a href="#" class="header_link">COMPANY</a></li>
+            </ul>
+        </nav>
+        <!-- お問い合わせ -->
+        <a href="#" class="header_button">お問い合わせ</a>
+    </header>
